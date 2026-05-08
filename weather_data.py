c9b2ca293fc790001540ab3788d27edc5d5fe33c
Katri Lotamõis <katrilotamois@outlook.com>
2026-05-08 12:08:55 +0300

    create class for data containment

---
+++ weather_data.py
@@ -0,0 +1,15 @@
+class WeatherData:
+    """Class for carrying the weather data."""
+
+
+    def __init__(self, city, temp, description, humidity):
+        self.city = city
+        self.temp = temp
+        self.description = description
+        self.humidity = humidity
+
+    def __str__(self):
+        return (f"--- Ilm linnas: {self.city} ---\n"
+                f"Temperatuur: {self.temp}°C\n"
+                f"Kirjeldus: {self.description.capitalize()}\n"
+                f"Õhuniiskus: {self.humidity}%")
